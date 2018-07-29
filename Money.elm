70e19ceb8b8be49af9a7d4e91c5cb86e1c7647fd
Brian Hicks <brian@brianthicks.com>
2018-07-28 21:19:51 -0500

    minimal money

---
+++ Money.elm
@@ -0,0 +1,9 @@
+module Money exposing (..)
+
+
+type Money
+    = Dollar
+    | Quarter
+    | Dime
+    | Nickel
+    | Penny
---
+++ tests/MoneySpec.elm
@@ -0,0 +1,12 @@
+module MoneySpec exposing (..)
+
+import Expect
+import Fuzz
+import Money exposing (..)
+import Test exposing (..)
+
+
+moneySpec : Test
+moneySpec =
+    describe "money"
+        []
